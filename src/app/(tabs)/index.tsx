5e5040f83c9ec54aad3940351208871c9103b1eb
Michał Bryk <galeriakoloru90210@gmail.com>
2024-02-07 23:53:18 +0100

    image render

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,24 +1,29 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 
 
 import EditScreenInfo from '@/src/components/EditScreenInfo';
 import Colors from '@/src/constants/Colors';
+import products from '@/assets/data/products'
+
+const product = products[0]
 
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Pizza Pepperoni</Text>
-      <Text style={styles.price}>$12.99</Text>
+      <Image source={{uri: product.image}} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    backgroundColor: 'white',
     alignItems: 'center',
-    padding: 20,
+    padding: 10,
+    borderRadius: 20
   },
   title: {
     fontSize: 18,
@@ -29,6 +34,9 @@ const styles = StyleSheet.create({
     color: Colors.light.tint,
     fontWeight: 'bold',
   },
-
+  image: {
+    width: '100%',
+    aspectRatio: 1
+  }
   
 });
