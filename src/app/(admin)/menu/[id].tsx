c29486dba744049ca77bc5594415bc9a4d8e45ef
Michał Bryk <galeriakoloru90210@gmail.com>
2024-02-11 12:08:57 +0100

    admin side structure

---
+++ src/app/(admin)/menu/[id].tsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { View, Text, Image, StyleSheet, Pressable, Alert } from 'react-native';
+import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
+
+import products from '@/assets/data/products';
+import Button from '@/src/components/Button';
+import { defaultProductImage } from '@/src/components/ProductListItem';
+import { useCart } from '@/src/providers/CartProvider';
+import { PizzaSize } from '@/src/types';
+
+const sizes: PizzaSize[] = ['S', 'M', 'L', 'XL'];
+
+const ProductDetailPage = () => {
+  const { id } = useLocalSearchParams();
+  const { addItem } = useCart();
+
+  const router = useRouter();
+
+  const [selectedSize, setSelectedSize] = useState<PizzaSize>('M');
+
+  const product = products.find(p => p.id.toString() === id);
+
+  const addToCart = () => {
+    if (!product) {
+      return;
+    }
+
+    addItem(product, selectedSize);
+    Alert.alert(
+      'Added to Cart',
+      'Do you want to continue shopping or go to the cart?',
+      [
+        {
+          text: 'Continue Shopping',
+          onPress: () => console.log('Continuing shopping'),
+        },
+        {
+          text: 'Go to Cart',
+          onPress: () => router.push('/cart'),
+        },
+      ],
+    );
+  };
+
+  if (!product) {
+    return (
+      <View>
+        <Stack.Screen options={{ title: 'Product not found' }} />
+        <Text>Product not found</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name }} />
+      <Image
+        source={{ uri: product.image || defaultProductImage }}
+        style={styles.image}
+      />
+
+      <Text style={styles.price}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+export default ProductDetailPage;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  },
+  price: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -2,7 +2,7 @@ import { StyleSheet, View, Text, Image, Pressable } from 'react-native';
 
 import Colors from '@/src/constants/Colors';
 import { Product } from '@/src/types';
-import { Link } from 'expo-router';
+import { Link, useSegments } from 'expo-router';
 
 type ProductListItemProps = {
   product: Product;
@@ -12,9 +12,11 @@ export const defaultProductImage =
   'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
 
 const ProductListItem = ({ product }: ProductListItemProps) => {
+  const segments = useSegments();
+
   return (
     //@ts-ignore
-    <Link href={`/menu/${product.id}`} asChild>
+    <Link href={`/${segments[0]}/menu/${product.id}`} asChild>
       <Pressable style={styles.container}>
         <Image
           source={{ uri: product.image || defaultProductImage }}
