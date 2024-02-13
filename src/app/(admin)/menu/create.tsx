b3bfd149d96df731317d0a9c1f8c1ce0ffa1c450
Michał Bryk <galeriakoloru90210@gmail.com>
2024-02-13 23:00:48 +0100

    create image picker

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,11 +1,17 @@
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Image } from 'react-native';
 import React, { useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+
 import Button from '@/src/components/Button';
+import { defaultProductImage } from '@/src/components/ProductListItem';
+import Colors from '@/src/constants/Colors';
+import { Stack } from 'expo-router';
 
 const CreateProductScreen = () => {
   const [name, setName] = useState<string>('');
   const [price, setPrice] = useState<string>('');
   const [errors, setErrors] = useState<string[]>([]);
+  const [image, setImage] = useState<string | null>(null);
 
   const resetFields = () => {
     setName('');
@@ -43,8 +49,30 @@ const CreateProductScreen = () => {
     resetFields();
   };
 
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
+      <Stack.Screen options={{ title: 'Create Product' }} />
+      <Image
+        source={{ uri: image || defaultProductImage }}
+        style={styles.image}
+      />
+      <Text onPress={pickImage} style={styles.textButton}>
+        Select Image
+      </Text>
       <Text style={styles.label}>Name</Text>
       <TextInput
         value={name}
@@ -82,6 +110,18 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginTop: 5,
     marginBottom: 20,
+  },
+  image: {
+    width: '40%',
+    aspectRatio: 1,
+    alignSelf: 'center',
+  },
+  textButton: {
+    color: Colors.light.tint,
+    fontSize: 16,
+    alignSelf: 'center',
+    marginBottom: 20,
+    marginVertical: 10,
   },
 });
 
